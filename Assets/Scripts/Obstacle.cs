13b18311863aa6ece665cc6e75e1ae27e1e26120
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2023-01-15 00:10:56 +0100

    Animations added
    Animations added to player, obstacle and collision

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     private GameObject player;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
     }
-
+    /*void Update ()
+    {
+        animator.SetFloat("exp",2);
+    }*/
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Border")
@@ -19,6 +23,7 @@ public class Obstacle : MonoBehaviour
         }
         else if (collision.tag == "Player")
         {
+            animator.SetFloat("exp", 2);
             Destroy(player.gameObject);
         }
     }
